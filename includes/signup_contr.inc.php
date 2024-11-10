58c7f93e4d294d2b1f8a948128c76e63e3cb384b
Gioseppe Alcoreza <gio@gioalco.com>
2024-11-11 01:36:34 +0800

    Half-baked sign-up function. Button works but still need to sql function to insert.

---
+++ includes/signup_contr.inc.php
@@ -0,0 +1,42 @@
+<?php
+
+// contr stands for Controller as MVC pattern
+
+//declare(strict_types=1);
+
+function is_input_empty($email, $pwd) {
+    if (empty($email) || empty($pwd)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function is_email_invalid($email) {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function is_username_taken($pdo, $username) {
+    if (get_username($pdo, $username)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function is_email_registered($pdo, $email) {
+    if (get_email($pdo, $email)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
