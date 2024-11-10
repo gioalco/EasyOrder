eb39ab3a4c0a06a55b21431216599b1965676d39
Gioseppe Alcoreza <gio@gioalco.com>
2024-11-11 02:52:50 +0800

    Working signup button. Though error handlers are not showing the kind of errors in html webpage but works by redirecting the webpage to signin.php again.
    All praise to God Jesus Christ!

--- includes/signup_model.inc.php
+++ includes/signup_model.inc.php
@@ -2,7 +2,7 @@
 
 //declare(strict_types=1);
 
-function get_username(object $pdo, string $username) {
+function get_username($pdo, $username) {
     $query = "SELECT username FROM users WHERE username = :username;";
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(":username", $username);
@@ -12,7 +12,7 @@ function get_username(object $pdo, string $username) {
     return $result;
 }
 
-function get_email(object $pdo, string $email) {
+function get_email($pdo, $email) {
     $query = "SELECT username FROM users WHERE email = :email;";
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(":email", $email);
@@ -20,4 +20,20 @@ function get_email(object $pdo, string $email) {
 
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     return $result;
+}
+
+function set_user($pdo, $pwd, $username, $email) {
+    $query = "INSERT INTO users (username, pwd, email) VALUES (:username, :pwd, :email);";
+    $stmt = $pdo->prepare($query);
+
+    $options = [
+        'cost' => 12,
+    ];
+
+    $hashedPwd = password_hash($pwd, PASSWORD_BCRYPT, $options);
+
+    $stmt->bindParam(":username", $username);
+    $stmt->bindParam(":pwd", $hashedPwd);
+    $stmt->bindParam(":email", $email);
+    $stmt->execute();
 }
