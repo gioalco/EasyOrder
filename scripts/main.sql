6725a4a2ca41c2ea703b1f64bc348bfe8a8c1c7b
Elmer Gioseppe Alcoreza <gio@gioalco.com>
2024-11-11 16:44:44 +0800

    Added admin and user in the test.sql
    Added profile into main.sql

--- scripts/main.sql
+++ scripts/main.sql
@@ -1,15 +1,27 @@
 CREATE DATABASE IF NOT EXISTS EasyOrder;
 USE EasyOrder;
 
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS person;
 
 CREATE TABLE person (
     id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(30) NOT NULL,
     pwd VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL,
-#     authority ENUM('user', 'admin'),
+    authority ENUM('user', 'admin') DEFAULT 'user',
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
   );
+
+CREATE TABLE profile (
+    id INT NOT NULL AUTO_INCREMENT,
+    person_id INT NOT NULL,
+    full_name VARCHAR(255),
+    phone VARCHAR(11),
+    address VARCHAR(255),
+    bio VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (person_id) REFERENCES person(id)
+);
 
