c2f00c581a539d3efdb4f99cca2763f753b1ef6a
Elmer Gioseppe Alcoreza <gio@gioalco.com>
2024-11-12 15:59:53 +0800

    Added empty 404 page, .htaccess, and contact page

---
+++ 404.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EasyOrder Restaurant</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
+    <script src="components/header.js"></script>
+    <script src="components/footer.js"></script>
+
+    <style></style>
+
+</head>
+<body>
+    <!--HEADER-->
+    <?php include 'components/header-public.php' ?>
+
+
+
+<!--    TODO: CHANGE THIS -->
+    <br><br><br> <p>Error</p>
+
+
+
+    <!-- FOOTER -->
+    <footer-component></footer-component>
+</body>
